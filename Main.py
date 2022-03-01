3c54bded27c05bcf9e0299132acbcc09df2d5352
Rathan <60232524+kokilarathan-siva01@users.noreply.github.com>
2022-03-01 23:06:07 +0000

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,17 +1,52 @@
 from cProfile import label
 from cgitb import text
+from glob import glob
 from struct import pack
 from tkinter import *
+from tkinter import messagebox
 
 
-def teacher_signup():
-      allUserID = userID.get()
-      allPassword = password.get()
+
+def employer_signup():
+      
+      authorisation_code = "123456789"
+      userIDinfo = userID.get()
+      passwordinfo = password.get()
+      emailinfo = email.get() 
+      codeinfo = code.get()
+      
+      if codeinfo == authorisation_code:
+            # Change this section later to connect with the database.
+            file = open(userIDinfo+".txt", "w")
+            file.write(userIDinfo)
+            file.write(passwordinfo)
+            file.write(emailinfo)
+            file.close()
+            
+            userID_ent.delete(0,END)
+            password_ent.delete(0,END)
+            email_ent.delete(0,END)
+            code_ent.delete(0,END)
+            
+            Label(screen_signup,text = "Sign Up Process Compeleted Successfully.", fg="green", font=("Ariel", 12)).pack()
+      else:
+            userID_ent.delete(0,END)
+            password_ent.delete(0,END)
+            email_ent.delete(0,END)
+            code_ent.delete(0,END)
+            Label(screen_signup,text = "Authorisation code is invalid. Please Try Again.", fg="red", font=("Ariel", 12)).pack()
+            
+      
+      
+      
+      
+      
 
 
 
 # Function which will enables the teacher to sign up
 def signUp():
+    global screen_signup
     screen_signup = Toplevel(main_Screen)
     screen_signup.title("Sign Up")
     screen_signup.geometry("700x700")
@@ -20,24 +55,39 @@ def signUp():
     
     global userID
     global password
-    global confirmPass
+    global email
+    global code
+    global userID_ent
+    global password_ent
+    global email_ent
+    global code_ent
+    
     userID = StringVar()
     password = StringVar()
-    confirmPass = StringVar()
+    email = StringVar()
+    code = StringVar()
     
     Label(screen_signup, text="User ID").pack()
     Label(screen_signup, text="").pack()
-    Entry(screen_signup, textvariable= userID).pack()
-    
+    userID_ent = Entry(screen_signup, textvariable= userID)
+    userID_ent.pack()
+
     Label(screen_signup, text= "Password").pack()
     Label(screen_signup, text="").pack()
-    Entry(screen_signup, textvariable= password).pack()
+    password_ent = Entry(screen_signup, textvariable= password)
+    password_ent.pack()
     
-    Label(screen_signup, text= "Confirm Password").pack()
+    Label(screen_signup, text= "email").pack()
     Label(screen_signup, text="").pack()
-    Entry(screen_signup, textvariable= confirmPass).pack()
+    email_ent = Entry(screen_signup, textvariable= email)
+    email_ent.pack()
     
-    Button(screen_signup, text ="Sign Up", height="2", width= "10").pack()
+    Label(screen_signup, text="Authorisation Code").pack()
+    Label(screen_signup, text="").pack()
+    code_ent = Entry(screen_signup, textvariable= code)
+    code_ent.pack()
+    
+    Button(screen_signup, text ="Sign Up", height="2", width= "10", command=employer_signup).pack()
     
     screen_signup.mainloop()
     
